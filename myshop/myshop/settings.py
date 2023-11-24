7e45a5145b9d6d74fcca83c16a3f1504e93a3e2e
BoyarkinAlexandr <generator185@gmail.com>
2023-11-24 15:19:16 +0300

    Хранение корзины покупок в сессиях

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CART_SESSION_ID = 'cart'
