6031126329cb3fed014a4ea283b5754bce52eb0f
BoyarkinAlexandr <generator185@gmail.com>
2023-11-24 12:14:05 +0300

    Регистрация моделей catalog на сайте администрирования

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,5 +1,7 @@
 # Create your models here.
 from django.db import models
+from django.core.urlresolvers import reverse
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
@@ -12,6 +14,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
@@ -31,3 +37,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
